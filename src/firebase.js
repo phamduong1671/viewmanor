c1871616532d6443478d156fd7457a2f6800a685
phamduong1671 <74924072+phamduong1671@users.noreply.github.com>
2022-11-27 19:24:45 +0700

    install firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBx0cy7dMw_J4QpJ6T68Rc2j9AZjD77rls",
+  authDomain: "viewmanor-1eafb.firebaseapp.com",
+  projectId: "viewmanor-1eafb",
+  storageBucket: "viewmanor-1eafb.appspot.com",
+  messagingSenderId: "1064579715767",
+  appId: "1:1064579715767:web:b1264eaf0f409a830c7332"
+};
+
+const app = initializeApp(firebaseConfig);
